3dce6d59cf0124d96e08b1b48be1deb560c25dd0
tiway <stilon670@gmail.com>
2024-05-06 15:44:34 +0300

    Created Newspaper model in models.py

--- redact_radar/models.py
+++ redact_radar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    dish_type = models.ForeignKey(
+        "Topic",
+        on_delete=models.CASCADE,
+        related_name="newspapers",
+    )
+    publishers = models.ManyToManyField("Redactor", related_name="newspaper")
+
+    class Meta:
+        ordering = ("title", )
+
+    def __str__(self):
+        return f"title: {self.title}, topic: {self.dish_type}"
