63d45f9f2445ecedc341d479ce65fbff2c14d6cb
tiway <stilon670@gmail.com>
2024-05-06 15:17:11 +0300

    Replacd single quotes with double quotes

--- redact_radar/apps.py
+++ redact_radar/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RedactRadarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'redact_radar'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "redact_radar"
