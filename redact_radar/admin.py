80f8bd3fe90c1acf23910f7ca289393d18792930
tiway <stilon670@gmail.com>
2024-05-07 12:58:09 +0300

    add clases topicadmin, redactoradmin, newspaperadmin in admin.py

--- redact_radar/admin.py
+++ redact_radar/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from redact_radar.models import Newspaper, Topic, Redactor
+
+admin.site.unregister(Group)
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "get_topics", "published_date", )
+    search_fields = ("title", )
+    ordering = ("-published_date",)
+
+    def get_topics(self, obj) -> str:
+        return ", ".join([topic.name for topic in obj.dish_type.all()])
+    get_topics.short_description = "Topics"
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name", )
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("User Information", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("User Information", {"fields": ("years_of_experience",)}),
+    )
+    search_fields = ("username", )
+    ordering = ("username", )
