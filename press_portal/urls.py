f361452199e35460a2f60f765d810a8c76021514
tiway <stilon670@gmail.com>
2024-05-08 12:32:08 +0300

    Installation and configuration of the Debug toolbar

--- press_portal/urls.py
+++ press_portal/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("redact_radar.urls", namespace="redact_radar"))
+    path("", include("redact_radar.urls", namespace="redact_radar")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
