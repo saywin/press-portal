d5e6f7acd6523d3a62095b6321764ebf58e4069c
tiway <stilon670@gmail.com>
2024-05-09 11:44:19 +0300

    add NewspaperDetailViews in views.py and path newspaper-detail in urls.py

--- redact_radar/urls.py
+++ redact_radar/urls.py
@@ -2,16 +2,18 @@ from django.urls import path
 
 from redact_radar.views import (
     IndexView,
-    NewspaperList,
-    TopicList,
-    RedactorList,
+    NewspaperListView,
+    TopicListView,
+    RedactorListView,
+    NewspaperDetailView,
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("newspaper/", NewspaperList.as_view(), name="newspaper-list"),
-    path("topic/", TopicList.as_view(), name="topic-list"),
-    path("redactor/", RedactorList.as_view(), name="redactor-list"),
+    path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("topic/", TopicListView.as_view(), name="topic-list"),
+    path("redactor/", RedactorListView.as_view(), name="redactor-list"),
+    path("newspaper/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail")
 ]
 
 app_name = "redact_radar"
