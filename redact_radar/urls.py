867f7f78e024c4f816487409bbb89fd86cd6d237
tiway <stilon670@gmail.com>
2024-05-09 16:01:00 +0300

    add path newspaper-create, newspaper-updete, newspaper-delete

--- redact_radar/urls.py
+++ redact_radar/urls.py
@@ -8,12 +8,18 @@ from redact_radar.views import (
     NewspaperDetailView,
     TopicDetailView,
     RedactorDetailView,
+    NewspaperCreateView,
+    NewspaperUpdateView,
+    NewspaperDeleteView,
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspaper/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspaper/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path("newspaper/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper-update"),
+    path("newspaper/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
     path("topic/", TopicListView.as_view(), name="topic-list"),
     path("topic/<int:pk>/", TopicDetailView.as_view(), name="topic-detail"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
