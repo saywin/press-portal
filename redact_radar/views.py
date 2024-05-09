a167b8126fa37a27de01ed7608f8b630c64c6a10
tiway <stilon670@gmail.com>
2024-05-09 11:45:37 +0300

    Created newspaper_detail.html and newspaper_detail.css

--- redact_radar/views.py
+++ redact_radar/views.py
@@ -15,21 +15,25 @@ class IndexView(generic.ListView):
         }
 
 
-class NewspaperList(generic.ListView):
+class NewspaperListView(generic.ListView):
     model = Newspaper
     template_name = "redact_radar/newspaper_list.html"
     context_object_name = "newspaper_list"
     paginate_by = 3
 
 
-class TopicList(generic.ListView):
+class NewspaperDetailView(generic.DetailView):
+    model = Newspaper
+
+
+class TopicListView(generic.ListView):
     model = Topic
     template_name = "redact_radar/topic_list.html"
     context_object_name = "topic_list"
     paginate_by = 4
 
 
-class RedactorList(generic.ListView):
+class RedactorListView(generic.ListView):
     model = Redactor
     template_name = "redact_radar/redactor_list.html"
     context_object_name = "redactor_list"
