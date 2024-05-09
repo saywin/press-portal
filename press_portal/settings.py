20ccb9808c8e7130db994b1a65891d2113d6334b
tiway <stilon670@gmail.com>
2024-05-09 14:47:59 +0300

    created page login.html and logged.html and add login.css

--- press_portal/settings.py
+++ press_portal/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,5 @@ INTERNAL_IPS = [
 ]
 
 LOGIN_REDIRECT_URL = "/newspaper/"
+LOGIN_URL = reverse_lazy("login")
+LOGOUT_URL = reverse_lazy("logout")
