202611d3aec48d90a5cf2b16bf3a789a3347c8fe
tiway <stilon670@gmail.com>
2024-05-13 16:56:07 +0300

    Write tests for topic in test_topic_view.py

--- redact_radar/tests/test_views/test_newspaper_views.py
+++ redact_radar/tests/test_views/test_newspaper_views.py
@@ -4,16 +4,27 @@ from django.urls import reverse
 
 from redact_radar.forms import NewspaperSearchForm
 from redact_radar.models import Topic, Newspaper
-from redact_radar.views import NewspaperListView, NewspaperCreateView, NewspaperUpdateView, NewspaperDeleteView
+from redact_radar.views import (
+    NewspaperListView,
+    NewspaperCreateView,
+    NewspaperUpdateView,
+    NewspaperDeleteView,
+)
 
 NEWSPAPER_URL = reverse("redact_radar:newspaper-list")
 
 
 class PublicNewspaperTests(TestCase):
     def test_newspaper_create_update_delete_login_required(self):
-        newspaper_create = self.client.get(reverse("redact_radar:newspaper-create"))
-        newspaper_update = self.client.get(reverse("redact_radar:newspaper-update", args=[1]))
-        newspaper_delete = self.client.get(reverse("redact_radar:newspaper-delete", args=[1]))
+        newspaper_create = self.client.get(
+            reverse("redact_radar:newspaper-create")
+        )
+        newspaper_update = self.client.get(
+            reverse("redact_radar:newspaper-update", args=[1])
+        )
+        newspaper_delete = self.client.get(
+            reverse("redact_radar:newspaper-delete", args=[1])
+        )
         self.assertEqual(newspaper_create.status_code, 302)
         self.assertEqual(newspaper_update.status_code, 302)
         self.assertEqual(newspaper_delete.status_code, 302)
@@ -34,12 +45,14 @@ class PrivateNewspaperTests(TestCase):
         self.response = self.client.get(NEWSPAPER_URL)
 
     def test_newspaper_create_login_required(self):
-        newspaper_create = self.client.get(reverse("redact_radar:newspaper-create"))
+        newspaper_create = self.client.get(
+            reverse("redact_radar:newspaper-create")
+        )
         self.assertEqual(newspaper_create.status_code, 200)
 
     def test_newspaper_update_login_required(self):
-        newspaper_update = self.client.get(reverse(
-            "redact_radar:newspaper-update", args=[self.newspaper.id])
+        newspaper_update = self.client.get(
+            reverse("redact_radar:newspaper-update", args=[self.newspaper.id])
         )
         self.assertEqual(newspaper_update.status_code, 200)
 
@@ -51,6 +64,18 @@ class PrivateNewspaperTests(TestCase):
 
     def test_newspaper_paginate(self):
         self.assertEqual(NewspaperListView.paginate_by, 3)
+
+    def test_topic_context(self):
+        newspaper = Newspaper.objects.all()
+        self.assertEqual(
+            list(self.response.context["newspaper_list"]),
+            list(newspaper)
+        )
+
+    def test_newspaper_template_used(self):
+        self.assertTemplateUsed(
+            self.response, "redact_radar/newspaper_list.html"
+        )
 
     def test_newspaper_context_data(self):
         form = self.response.context["search_form"]
---
+++ redact_radar/tests/test_views/test_topic_view.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from redact_radar.models import Topic
+from redact_radar.views import (
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
+)
+
+TOPIC_URL = reverse("redact_radar:topic-list")
+
+
+class PublicTopicViewTests(TestCase):
+    def test_login_required(self):
+        res = self.client.get(TOPIC_URL)
+        self.assertEqual(res.status_code, 302)
+
+
+class PrivateTopicViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="test123",
+        )
+        self.client.force_login(self.user)
+        self.topic = Topic.objects.create(name="sport")
+        self.response = self.client.get(TOPIC_URL)
+
+    def test_login_required(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_topic_template_used(self):
+        self.assertTemplateUsed(self.response, "redact_radar/topic_list.html")
+
+    def test_topic_context(self):
+        manufacturers = Topic.objects.all()
+        self.assertEqual(
+            list(self.response.context["topic_list"]),
+            list(manufacturers)
+        )
+
+    def test_topic_paginate(self):
+        self.assertEqual(TopicListView.paginate_by, 4)
+
+    def test_topic_create_success_url(self):
+        view = TopicCreateView()
+        view.object = self.topic
+        self.assertEqual(view.get_success_url(), TOPIC_URL)
+
+    def test_topic_update_success_url(self):
+        view = TopicUpdateView()
+        view.object = self.topic
+        self.assertEqual(view.get_success_url(), TOPIC_URL)
+
+    def test_topic_delete_success_url(self):
+        view = TopicDeleteView()
+        view.object = self.topic
+        self.assertEqual(view.get_success_url(), TOPIC_URL)
