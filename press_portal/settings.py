63c7811a8dd65d551ffbc90902877ccc6149ea55
tiway <stilon670@gmail.com>
2024-05-15 16:15:09 +0300

    Remove insecure django secret key for deployment

--- press_portal/settings.py
+++ press_portal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-+4gv5n(+iijesg#p^=m8q(b65)w_*--ln--ut_e9d4z-ps=c19")
+SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
