e2f83e22419201eb9412733302b6d3732b4929f1
tiway <stilon670@gmail.com>
2024-05-15 16:39:44 +0300

    fix secret key

--- press_portal/settings.py
+++ press_portal/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ]
 
