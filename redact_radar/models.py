62756cc9914dfaca3b490a6dd6bf84304058ada2
tiway <stilon670@gmail.com>
2024-05-16 10:37:20 +0300

    changed sorting by date

--- redact_radar/models.py
+++ redact_radar/models.py
@@ -19,7 +19,7 @@ class Newspaper(models.Model):
     )
 
     class Meta:
-        ordering = ("published_date",)
+        ordering = ("-published_date",)
 
     def __str__(self):
         return f"title: {self.title}, topic: {self.dish_type}"
